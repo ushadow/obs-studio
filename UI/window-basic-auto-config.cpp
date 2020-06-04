5d2af05767cbd90172439fe06efd4b7e77811a14
Maya Venkatraman <mvenkat@google.com>
2020-06-04 11:38:39 -0400

    UI: Change or into ||
    Change or keyword to || to be more consistent
    with the rest of the codebase. Also change Youtube service
    names in services.json to have consistent spacing.

--- UI/window-basic-auto-config.cpp
+++ UI/window-basic-auto-config.cpp
@@ -566,7 +566,7 @@ void AutoConfigStreamPage::UpdateKeyLink()
 	if (serviceName == "Twitch") {
 		streamKeyLink =
 			"https://www.twitch.tv/broadcast/dashboard/streamkey";
-	} else if ((serviceName == "YouTube - RTMP") or
+	} else if ((serviceName == "YouTube - RTMP") ||
 		   (serviceName == "YouTube - HLS")) {
 		streamKeyLink = "https://www.youtube.com/live_dashboard";
 		isYoutube = true;
