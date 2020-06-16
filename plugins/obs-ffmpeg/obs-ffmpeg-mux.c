d06d3758cc0160fb10bc6be2d7c2ddff89027be5
Maya Venkatraman <mvenkat@google.com>
2020-06-16 16:08:23 -0400

    obs-ffmpeg: Fix http_user_agent settings
    Add a call to dstr_init in the hls_start_mux function so that
    every time hls_start_mux is called muxer_settings is zeroed
    and can be set again.

--- plugins/obs-ffmpeg/obs-ffmpeg-mux.c
+++ plugins/obs-ffmpeg/obs-ffmpeg-mux.c
@@ -388,6 +388,7 @@ static bool ffmpeg_hls_mux_start(void *data)
 	stream_key = obs_service_get_key(service);
 	dstr_copy(&path, path_str);
 	dstr_replace(&path, "{stream_key}", stream_key);
+	dstr_init(&stream->muxer_settings);
 	dstr_catf(&stream->muxer_settings, "http_user_agent=libobs/%s",
 		  OBS_VERSION);
 
